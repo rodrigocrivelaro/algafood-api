69a4ca738f14d68cc685470c9849f93a64e38f6f
Rodrigo Crivelaro dos Reis <rreiscrivelaro@gmail.com>
2021-09-08 22:19:04 -0300

    Modelando e requisitando um Collection Resource com GET;Desafio: collection resource de estados;Implementando content negotiation para retornar JSON ou XML;Consultando Singleton Resource com GET e @PathVariable;Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName;Customizando a representação em XML com Wrapper e anotações do Jackson;

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cozinhas"/*, produces = MediaType.APPLICATION_JSON_VALUE*/)
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping( produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml() {
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.buscar(cozinhaId);
+    }
+
+}
